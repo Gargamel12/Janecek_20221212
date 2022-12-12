d9c58dad396046618b7b03c40e22a79074c5bcd9
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2022-12-12 12:22:43 +0100

    Add project files.

---
+++ Soldier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janecek_20221212
+{
+    public class Soldier
+    {
+        private int 
+
+        public string Jmeno { get; set; }
+        public string HP { get; set; }
+        public string MaxHP { get; set; }
+        public string DMG { get; set; }
+        public string Shield { get; set; }
+        public string MaxShield { get; set; }
+        public Soldier(int Jmeno; int maxhp;int)
+        {
+            Jmeno ="";
+            
+        }
+
+        public void Attack(Enemy Soldier)
+    }
+
+    
+}
