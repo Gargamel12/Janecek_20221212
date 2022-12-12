80332d917a1915203b01cc87a0538468acd7c2c6
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2022-12-12 13:04:30 +0100

    Brekeke

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,16 +13,163 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Janecek_20221212
+namespace janecek_20221212
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        Soldier soldier1;
+        Soldier soldier2;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            soldier1 = new Soldier();
+            soldier1.Jmeno = "Soldier 76";
+            Zobraz(soldier1, txt1);
+
+            soldier2 = new Soldier();
+            soldier2.Jmeno = "Soldier 67";
+            Zobraz(soldier2, txt2);
+        }
+
+        public void Zobraz(Soldier soldier, TextBox textBox)
+        {
+            textBox.Text = soldier.Jmeno + "\n";
+            textBox.Text += "Život: " + soldier.Zivot.ToString() + "\n";
+            textBox.Text += "MaxHP:" + soldier.MaxHP.ToString() + "/n"; 
+            textBox.Text += "Dovednost: " + soldier.Dovednost.ToString() + "\n";
+            textBox.Text += "Volné healy: " + soldier.Heals.ToString() + "\n";
+            textBox.Text += "DMG: " + soldier.Dmg.ToString() + "\n";
+        }
+        public void dead(Soldier clovek, TextBox textbox)
+        {
+            textbox.Text = clovek.Jmeno + " je mrtev";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            soldier1.PosunoutCas();
+            soldier2.PosunoutCas();
+            Zobraz(soldier1, txt1);
+            Zobraz(soldier2, txt2);
+            if (soldier1.Zivot <= 0)
+            {
+                dead(soldier1, txt1);
+            }
+            if (soldier2.Zivot <= 0)
+            {
+                dead(soldier2, txt2);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            soldier1.ZvysitDovednost();
+            Zobraz(soldier1, txt1);
+            if (soldier1.Zivot <= 0)
+            {
+                dead(soldier1, txt1);
+            }
+            if (soldier2.Zivot <= 0)
+            {
+                dead(soldier2, txt2);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            soldier2.Attack();
+            Zobraz(soldier2, txt2);
+            soldier1.Enerzy();
+            Zobraz(soldier1, txt1);
+            if (soldier1.Zivot <= 0)
+            {
+                dead(soldier1, txt1);
+                Window win2 = new Window();
+                win2.Show();
+            }
+            if (soldier2.Zivot <= 0)
+            {
+                dead(soldier2, txt2);
+                Window win1 = new Window();
+                win1.Show();
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            soldier1.Attack();
+            Zobraz(soldier1, txt1);
+            soldier2.Enerzy();
+            Zobraz(soldier2, txt2);
+            if (soldier2.Zivot <= 0)
+            {
+                dead(soldier2, txt2);
+                Window win1 = new Window();
+                win1.Show();
+            }
+            if (soldier1.Zivot <= 0)
+            {
+                dead(soldier1, txt1);
+                Window win2 = new Window();
+                win2.Show();
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            soldier1.Heal();
+            Zobraz(soldier1, txt1);
+
+
+            if (soldier1.Zivot <= 0)
+            {
+                dead(soldier1, txt1);
+
+            }
+            if (soldier2.Zivot <= 0)
+            {
+                dead(soldier2, txt2);
+
+            }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            jan.Drink();
+            Zobraz(jan, txt2);
+            if (karel.Zivot <= 0)
+            {
+                dead(karel, txt1);
+            }
+            if (jan.Zivot <= 0)
+            {
+                dead(jan, txt2);
+            }
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Window win2 = new Window();
+            win2.Show();
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            jan.ZvysitDovednost();
+            Zobraz(jan, txt2);
+            if (jan.Zivot <= 0)
+            {
+                dead(jan, txt2);
+            }
+            if (karel.Zivot <= 0)
+            {
+                dead(karel, txt1);
+            }
         }
     }
 }
--- Soldier.cs
+++ Soldier.cs
@@ -8,22 +8,113 @@ namespace Janecek_20221212
 {
     public class Soldier
     {
-        private int 
+        // soukromé proměnné - nejsou dostupné mimo třídu Clovek
+        private int energie;
+        private int dovednost;
+        private int drinks;
+        private int momentalnienerzy;
+        private int dmg;
 
+        // vlastnosti - zveřejňují hodnoty ven z třídy
         public string Jmeno { get; set; }
-        public string HP { get; set; }
-        public string MaxHP { get; set; }
-        public string DMG { get; set; }
-        public string Shield { get; set; }
-        public string MaxShield { get; set; }
-        public Soldier(int Jmeno; int maxhp;int)
+        public string Prijmeni { get; set; }
+        public DateTime DatumNarozeni { get; set; }
+        public int Energie { get => energie; }
+        public int Zivot { get; set; }
+        public int Dovednost { get => dovednost; }
+        public int Drinks { get => drinks; }
+        public int Dmg { get => dmg; }
+
+
+        //konstruktor - pomocí této metody vznikne konkrétní objekt - instance třídy
+        public Clovek()
         {
-            Jmeno ="";
-            
+            Jmeno = "";
+            Prijmeni = "";
+            DatumNarozeni = DateTime.Now;
+            energie = 100;
+            Zivot = 100;
+            dovednost = 0;
+            drinks = 5;
+            dmg = 0;
         }
 
-        public void Attack(Enemy Soldier)
-    }
 
-    
+        public void PosunoutCas()
+        {
+            energie += 20;
+            Zivot += 5;
+        }
+
+        public void ZvysitDovednost()
+        {
+            Zivot += 20;
+            dovednost += 10;
+            energie += 10;
+            dmg += 10;
+        }
+        public void Attack()
+        {
+            if (energie < 0)
+            {
+                Zivot -= 0;
+            }
+            else if (energie > 0)
+            {
+                Zivot -= 10;
+                Zivot -= Dmg;
+            }
+            else if (energie < 0)
+            {
+                Zivot = 0;
+            }
+            else if (Zivot <= 0)
+            {
+                Zivot -= 0;
+            }
+
+        }
+        public void Enerzy()
+        {
+            energie -= 20;
+            if (energie < 0)
+            {
+                energie = 0;
+                Zivot -= 5;
+            }
+            else if (Zivot <= 0)
+            {
+                Zivot -= 0;
+            }
+
+        }
+        public void Drink()
+        {
+            if (Zivot > 0)
+            {
+                momentalnienerzy = energie;
+                if (energie > 100)
+                {
+                    energie = 100;
+
+                }
+                if (drinks <= 0)
+                {
+                    energie = momentalnienerzy;
+                    drinks = 0;
+                }
+                if (energie < 100)
+                {
+                    energie += 50;
+                    drinks -= 1;
+                }
+            }
+            else
+            {
+                energie = -0;
+            }
+
+
+        }
+    }
 }
